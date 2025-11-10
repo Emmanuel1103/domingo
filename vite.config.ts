046b32b7ea134545d7cac25a59e51fc1e17d0a84
Emmanuel Morales <emorales14@cuc.edu.co>
2025-11-09 22:50:54 -0500

    Revert "Removed lovable-tagger dependency"
    This reverts commit cca3869591070bdf74b8243912269d4455528478.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/domingo/',  
+  base: mode === 'development' ? '/' : '/domingo/',  // Usa '/' en desarrollo y '/domingo/' en producción
 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
